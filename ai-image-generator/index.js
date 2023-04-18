9befbb379204ebf01fa4fcf34ee226fdcb426bba
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-04-18 11:19:54 -0400

    add css and html

--- ai-image-generator/index.js
+++ ai-image-generator/index.js
@@ -1,14 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
 import { OpenAIRouter } from './src/routes/OpenAI.routes.js'
+import { getDirName } from './src/utils/utils.js'
 
 dotenv.config()
 const port = process.env.PORT || 5000
 
 const app = express()
 
+const dirname = getDirName(import.meta.url)
+console.log(dirname)
+
 // Enable body parser
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.static(path.join(dirname, 'public')))
 
 app.use(OpenAIRouter)
 
