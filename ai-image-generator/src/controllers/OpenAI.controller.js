4444ba8070bdf4434faba8df7e3209402c49b98f
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-04-18 10:24:12 -0400

    connect api key

--- ai-image-generator/src/controllers/OpenAI.controller.js
+++ ai-image-generator/src/controllers/OpenAI.controller.js
@@ -1,4 +1,7 @@
 import { Configuration, OpenAIApi } from 'openai'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -8,9 +11,26 @@ const openai = new OpenAIApi(configuration)
 export const GenerateImage = async (req, res) => {
   try {
     const response = await openai.createImage({
-      prompt: 'Achilles and Athena',
+      prompt: 'Polar bear on ice saktes',
       n: 1,
       size: '512x512',
     })
-  } catch (error) {}
+    const imageUrl = response.data.data[0].url
+
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    })
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status)
+      console.log(error.response.data)
+    } else {
+      console.log(error.message)
+    }
+    res.status(400).json({
+      success: false,
+      error: 'The image could not be generated',
+    })
+  }
 }
