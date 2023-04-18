a5e749e39daabf34da30d525e0d834fded1fc9b3
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-04-18 10:47:04 -0400

    add req.body

--- ai-image-generator/index.js
+++ ai-image-generator/index.js
@@ -6,6 +6,10 @@ dotenv.config()
 const port = process.env.PORT || 5000
 
 const app = express()
+
+// Enable body parser
+app.use(express.json())
+
 app.use(OpenAIRouter)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
--- ai-image-generator/src/controllers/OpenAI.controller.js
+++ ai-image-generator/src/controllers/OpenAI.controller.js
@@ -9,11 +9,16 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 export const GenerateImage = async (req, res) => {
+  const { prompt, size } = req.body
+
+  const imageSize =
+    size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024'
+
   try {
     const response = await openai.createImage({
-      prompt: 'Polar bear on ice saktes',
+      prompt: prompt,
       n: 1,
-      size: '512x512',
+      size: imageSize,
     })
     const imageUrl = response.data.data[0].url
 
